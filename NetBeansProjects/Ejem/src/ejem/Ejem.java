6c42f15cb2f1a865c4be893f0bc7be9656ca8b6e
David Elias Abalco Maila <davidelias_am@hotmail.com>
2012-10-25 18:14:52 -0500

    nuevo

---
+++ NetBeansProjects/Ejem/src/ejem/Ejem.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejem;
+import java.util.Stack;
+/**
+ *
+ * @author David
+ */
+public class Ejem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String expr = "Es hora de bailar 1 2 3 4....!!!";
+        System.out.println("Ejemplo de cambio de subversion: "+expr);
+        System.out.println("Hola mundo!!!");
+    }
+}
